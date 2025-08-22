ce94958921bc3e56f452978b8cc7ccc70f518c95
DeepDarkBoy48 <warghost200818@gmail.com>
2025-08-22 12:43:43 +0800

    配置文件更新，连接 docker 网络 appnet

--- src/main/java/robin/discordbot/utils/ThreadLocalUtil.java
+++ src/main/java/robin/discordbot/utils/ThreadLocalUtil.java
@@ -8,8 +8,10 @@ public class ThreadLocalUtil {
     private static final ThreadLocal THREAD_LOCAL = new ThreadLocal();
 
     //根据键获取值
-    public static <T> T get(){
-        return (T) THREAD_LOCAL.get();
+    public static <T> T  get(){
+        Object o = THREAD_LOCAL.get();
+        T t = (T) o; //强制转换
+        return t;
     }
 
     //存储键值对
